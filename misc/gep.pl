44c487977ce93de39991fc1464bca79c9f038fda
Lorenz Köhl <lorenz@quub.de>
2023-11-28 23:56:37 +0100

    Fix file paths and update markdown formatting

--- misc/gep.pl
+++ misc/gep.pl
@@ -2,6 +2,7 @@
 :- use_module(library(clpz)).
 :- use_module(library(lists)).
 :- use_module(library(debug)).
+:- use_module(library(dcgs)).
 
 
 
@@ -18,4 +19,26 @@ random_gene(Gene) :-
     random_integer(0, 10, Gene).
 
 %?- random_gene(23, G).
-%?- init_chromosome(23, C)
+%?- init_chromosome(23, C).
+%@    C = [1,4,4,2,1,8,4,4,2,1].
+
+kexpr(Term) --> function(Term).
+kexpr(Term) --> terminal(Term).
+
+function(FuncTerm) --> ["+"], { FuncTerm =.. ['+', A, B] }, kexpr(A), kexpr(B).
+function(FuncTerm) --> ["*"], { FuncTerm =.. ['*', A, B] }, kexpr(A), kexpr(B).
+function(FuncTerm) --> ["sin"], { FuncTerm =.. ['sin', A] }, kexpr(A).
+
+terminal(a) --> ["a"].
+terminal(b) --> ["b"].
+
+eval_expr(a, 1).
+eval_expr(b, 2).
+eval_expr(+(A, B), Result) :- eval_expr(A, RA), eval_expr(B, RB), Result is RA + RB.
+eval_expr(*(A, B), Result) :- eval_expr(A, RA), eval_expr(B, RB), Result is RA * RB.
+eval_expr(sin(A), Result) :- eval_expr(A, RA), Result is sin(RA).
+
+%?- phrase("+*abab", Term)
+
+, eval_expr(Term, Result).
+%@    false.
